bb3fb48f3e6a068a164afd51b137f25ccc42e379
PolinaNiko <polinanikolaychuk@gmail.com>
2024-09-12 18:03:42 +0300

    3 commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello new program!");
         System.out.println("Another one commit!");
+        System.out.println("comm");
+        System.out.println("3 commit!");
     }
 }
